499bf5a4757016cb7559df09e6f5f112bea3331e
Exfridos <Exfridos@gmail.com>
2015-05-13 10:56:50 +0200

    Create Dontbet.user.js

---
+++ Dontbet.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Please don't bet Annie
+// @version      0.1
+// @author       Exfridos
+// @match        http://csgolounge.com/match?*
+// @match        https://csgolounge.com/match?*
+// @grant        none
+// ==/UserScript==
+
+$('a#placebut').text("Please don't bet Annie :c");
